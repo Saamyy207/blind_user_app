cda4997089c09d4fcf20af600197db97b3d4888c
Saamyy207 <samyaelhanchir@gmail.com>
2025-05-10 19:05:24 +0200

    added a little change

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home:  BlindProfileView( userId: '4fc39bc9-dfa4-4164-93b9-bcafe6034955',), // Change ici pour tester la caméra
+      home:  BlindProfileView( userId: 'e6cb9f1a-4e65-4d88-849d-884980803a0c',), // Change ici pour tester la caméra
     );
   }
 }
